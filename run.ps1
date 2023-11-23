1f560398980a2128f503abb56d01ff2b0a0785da
Stribx <parleapapie@gmail.com>
2023-11-23 19:08:51 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,4 +1,23 @@
-composer install
-npm install
-npm run dev
-symfony server:start --no-tls --port=8000
+# .\run.ps1 install
+function InstallDependencies {
+    Write-Host "Installing Composer dependencies..."
+    composer install
+    Write-Host "Installing npm dependencies..."
+    npm install
+}
+
+# .\run.ps1 run
+function RunDevelopment {
+    Write-Host "Running npm dev..."
+    npm run dev
+    Write-Host "Starting Symfony server..."
+    symfony server:start --no-tls --port=8000
+}
+
+$command = $args[0]
+
+switch ($command) {
+    "install" { InstallDependencies }
+    "run" { RunDevelopment }
+    default { Write-Host "Invalid command. Supported commands: install, run" }
+}
