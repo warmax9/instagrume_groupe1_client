dbcdcb23dfc24d144d7ad5afbb295e5802c8a409
Stribx <parleapapie@gmail.com>
2023-11-26 20:01:35 +0100

    css

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -7,14 +7,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/auth', name: 'auth')]
+#[Route('/auth')]
 class AuthController extends AbstractController
 {
     public function __construct(private Api $api)
     {
     }
 
-    #[Route('/', name: 'home', methods: ['GET'])]
+    #[Route('/', name: 'auth', methods: ['GET'])]
     public function auth(): Response
     {
         return $this->render('auth/auth.html.twig');
