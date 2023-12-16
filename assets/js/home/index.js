dd7436553163ed76d216412c89999b35e8126786
Stribx <parleapapie@gmail.com>
2023-12-16 18:43:37 +0100

    Update index.js

--- assets/js/home/index.js
+++ assets/js/home/index.js
@@ -18,8 +18,6 @@ function send(value, element, icon) {
     let url = (value.post_id !== undefined) ? '/like/post' : '/like/commentaire';
     fetch(url, {
         method: 'POST',
-        headers: {
-        },
         body: JSON.stringify(value),
     })
         .then(response => response.json())
@@ -33,22 +31,19 @@ function send(value, element, icon) {
                         icon.style.color = "red";
                         break;
                     case "modification":
+                        const divParent =  element.parentElement.parentElement;
                         if(element.classList.contains('cpt-like')){
-                            var divParent =  element.parentElement.parentElement;
-                            var otherCpt = divParent.querySelector(".cpt-dislike");
+                            const otherCpt = divParent.querySelector(".cpt-dislike");
                             otherCpt.textContent = parseInt(otherCpt.textContent, 10) - 1;
                             otherIcon = divParent.querySelector('ion-icon.dislike');
-                            otherIcon.removeAttribute('color');
                             otherIcon.style.color = "white";
                             element.textContent = currentNumber + 1;
                             icon.style.color = "red";
                         }
                         else{
-                            var divParent =  element.parentElement.parentElement;
-                            var otherCpt = divParent.querySelector(".cpt-like");
+                            const otherCpt = divParent.querySelector(".cpt-like");
                             otherCpt.textContent = parseInt(otherCpt.textContent, 10) - 1;
                             otherIcon = divParent.querySelector('ion-icon.like');
-                            otherIcon.removeAttribute('color');
                             otherIcon.style.color = "white";
                             element.textContent = currentNumber + 1;
                             icon.style.color = "red";
